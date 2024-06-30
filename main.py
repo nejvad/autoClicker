a0ba02776e2dc6020028ed959ecce2dc3ab67037
nejvad <jenesesdavid620@gmail.com>
2024-06-30 02:59:47 +0200

    create basic empty window with Tkinter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    return ""
+from tkinter import *
 
+master = Tk()
+master.geometry("800x800")
+master.title("Basic Autoclicker")
 
-if __name__ == "__main__":
-    main()
+mainloop()
